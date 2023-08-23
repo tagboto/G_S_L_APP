dfef1bb03f7eb305413aef3c079aec5062c5c45a
tagboto <zoe2.tagboto@gmail.com>
2023-08-23 21:46:04 +0100

    fix the pronouns page

--- lib/pages/prounoun_words_page/prounoun_words_page_widget.dart
+++ lib/pages/prounoun_words_page/prounoun_words_page_widget.dart
@@ -1,11 +1,11 @@
 import '/backend/backend.dart';
 import '../../gsl_flow/gsl_flow_animations.dart';
-import '../../gsl_flow/gsl_flow_theme.dart';
 import '../../gsl_flow/gsl_flow_util.dart';
 import '../../gsl_flow/gsl_flow_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProunounWordsPage extends StatefulWidget {
   const ProunounWordsPage({Key? key}) : super(key: key);
@@ -28,15 +28,15 @@ class _ProunounWordsPageState extends State<ProunounWordsPage>
           curve: Curves.easeInOut,
           delay: 0.ms,
           duration: 600.ms,
-          begin: 0.0,
-          end: 1.0,
+          begin: 0,
+          end: 1,
         ),
         MoveEffect(
           curve: Curves.easeInOut,
           delay: 0.ms,
           duration: 600.ms,
-          begin: Offset(30.0, 0.0),
-          end: Offset(0.0, 0.0),
+          begin: Offset(30, 0),
+          end: Offset(0, 0),
         ),
       ],
     ),
@@ -47,15 +47,15 @@ class _ProunounWordsPageState extends State<ProunounWordsPage>
           curve: Curves.easeInOut,
           delay: 0.ms,
           duration: 600.ms,
-          begin: 0.0,
-          end: 1.0,
+          begin: 0,
+          end: 1,
         ),
         MoveEffect(
           curve: Curves.easeInOut,
           delay: 0.ms,
           duration: 600.ms,
-          begin: Offset(50.0, 0.0),
-          end: Offset(0.0, 0.0),
+          begin: Offset(50, 0),
+          end: Offset(0, 0),
         ),
       ],
     ),
@@ -75,6 +75,7 @@ class _ProunounWordsPageState extends State<ProunounWordsPage>
 
   @override
   void dispose() {
+
     super.dispose();
   }
 
@@ -82,53 +83,57 @@ class _ProunounWordsPageState extends State<ProunounWordsPage>
   Widget build(BuildContext context) {
     context.watch<GSLAppState>();
 
-    return StreamBuilder<List<PronounsRecord>>(
-      stream: queryPronounsRecord(
-        queryBuilder: (pronounsRecord) => pronounsRecord.orderBy('signName'),
+    return StreamBuilder<List<NumbersRecord>>(
+      stream: queryNumbersRecord(
+        queryBuilder: (numbersRecord) => numbersRecord.orderBy('signName'),
         singleRecord: true,
       ),
       builder: (context, snapshot) {
         // Customize what your widget looks like when it's loading.
         if (!snapshot.hasData) {
           return Scaffold(
-            backgroundColor: GSLAppTheme.of(context).primaryBackground,
+            backgroundColor: Color(0xFFF1F4F8),
             body: Center(
               child: SizedBox(
-                width: 50.0,
-                height: 50.0,
+                width: 50,
+                height: 50,
                 child: CircularProgressIndicator(
                   valueColor: AlwaysStoppedAnimation<Color>(
-                    GSLAppTheme.of(context).primary,
+                    Color(0xFF4B39EF),
                   ),
                 ),
               ),
             ),
           );
         }
-        List<PronounsRecord> prounounWordsPagePronounsRecordList =
-            snapshot.data!;
+        List<NumbersRecord> prounounWordsPageNumbersRecordList = snapshot.data!;
         // Return an empty Container when the item does not exist.
         if (snapshot.data!.isEmpty) {
           return Container();
         }
-        final prounounWordsPagePronounsRecord =
-            prounounWordsPagePronounsRecordList.isNotEmpty
-                ? prounounWordsPagePronounsRecordList.first
+        final prounounWordsPageNumbersRecord =
+            prounounWordsPageNumbersRecordList.isNotEmpty
+                ? prounounWordsPageNumbersRecordList.first
                 : null;
         return GestureDetector(
           child: Scaffold(
             key: scaffoldKey,
-            backgroundColor: GSLAppTheme.of(context).primaryBackground,
+            backgroundColor: Color(0xFFF1F4F8),
             appBar: AppBar(
-              backgroundColor: GSLAppTheme.of(context).primary,
+              backgroundColor: Color(0xFF4B39EF),
               automaticallyImplyLeading: true,
               title: Text(
                 GSLAppState().categoryName,
-                style: GSLAppTheme.of(context).titleMedium,
+                style:GoogleFonts.getFont(
+        'Readex Pro',
+        color: Color(0xFFFFFFFF),
+        fontWeight: FontWeight.normal,
+        fontSize: 18.0,
+      ),
               ),
               actions: [],
               centerTitle: true,
-              elevation: 4.0,
+              elevation: 4,
             ),
             body: Column(
               mainAxisSize: MainAxisSize.max,
@@ -139,21 +144,20 @@ class _ProunounWordsPageState extends State<ProunounWordsPage>
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(
-                            16.0, 12.0, 0.0, 0.0),
+                        padding: EdgeInsetsDirectional.fromSTEB(16, 12, 0, 0),
                         child: Text(
                           'Scroll to see a list of new words',
                           style: TextStyle(
                                 fontFamily: 'Outfit',
-                                fontSize: 18.0,
+                                fontSize: 18,
                               ),
                         ),
                       ),
                       Container(
                         width: double.infinity,
-                        height: 496.0,
+                        height: 496,
                         decoration: BoxDecoration(
-                          color: GSLAppTheme.of(context).primaryBackground,
+                          color: Color(0xFFF1F4F8),
                         ),
                         child: StreamBuilder<List<PronounsRecord>>(
                           stream: queryPronounsRecord(
@@ -165,11 +169,11 @@ class _ProunounWordsPageState extends State<ProunounWordsPage>
                             if (!snapshot.hasData) {
                               return Center(
                                 child: SizedBox(
-                                  width: 50.0,
-                                  height: 50.0,
+                                  width: 50,
+                                  height: 50,
                                   child: CircularProgressIndicator(
                                     valueColor: AlwaysStoppedAnimation<Color>(
-                                      GSLAppTheme.of(context).primary,
+                                      Color(0xFF4B39EF),
                                     ),
                                   ),
                                 ),
@@ -184,22 +188,24 @@ class _ProunounWordsPageState extends State<ProunounWordsPage>
                               scrollDirection: Axis.horizontal,
                               itemCount: listViewPronounsRecordList.length,
                               itemBuilder: (context, listViewIndex) {
+                                final listViewPronounsRecord =
+                                    listViewPronounsRecordList[listViewIndex];
                                 return Padding(
                                   padding: EdgeInsetsDirectional.fromSTEB(
-                                      16.0, 12.0, 12.0, 12.0),
+                                      16, 12, 12, 12),
                                   child: Container(
-                                    width: 350.0,
-                                    height: 613.0,
+                                    width: 350,
+                                    height: 613,
                                     decoration: BoxDecoration(
                                       color: Color(0xFFFFFFFF),
                                       boxShadow: [
                                         BoxShadow(
-                                          blurRadius: 4.0,
+                                          blurRadius: 4,
                                           color: Color(0x34090F13),
-                                          offset: Offset(0.0, 2.0),
+                                          offset: Offset(0, 2),
                                         )
                                       ],
-                                      borderRadius: BorderRadius.circular(12.0),
+                                      borderRadius: BorderRadius.circular(12),
                                       shape: BoxShape.rectangle,
                                     ),
                                     child: Column(
@@ -207,24 +213,23 @@ class _ProunounWordsPageState extends State<ProunounWordsPage>
                                       children: [
                                         Container(
                                           width: double.infinity,
-                                          height: 302.0,
+                                          height: 302,
                                           decoration: BoxDecoration(
                                             color: Color(0x1F050505),
                                             borderRadius: BorderRadius.only(
-                                              bottomLeft: Radius.circular(0.0),
-                                              bottomRight: Radius.circular(0.0),
-                                              topLeft: Radius.circular(12.0),
-                                              topRight: Radius.circular(12.0),
+                                              bottomLeft: Radius.circular(0),
+                                              bottomRight: Radius.circular(0),
+                                              topLeft: Radius.circular(12),
+                                              topRight: Radius.circular(12),
                                             ),
                                           ),
                                           child: ClipRRect(
                                             borderRadius:
-                                                BorderRadius.circular(8.0),
+                                                BorderRadius.circular(8),
                                             child: Image.network(
-                                              prounounWordsPagePronounsRecord!
-                                                  .signImage,
-                                              width: 300.0,
-                                              height: 200.0,
+                                              listViewPronounsRecord.signImage,
+                                              width: 300,
+                                              height: 200,
                                               fit: BoxFit.cover,
                                             ),
                                           ),
@@ -232,7 +237,7 @@ class _ProunounWordsPageState extends State<ProunounWordsPage>
                                         Padding(
                                           padding:
                                               EdgeInsetsDirectional.fromSTEB(
-                                                  12.0, 12.0, 12.0, 0.0),
+                                                  12, 12, 12, 0),
                                           child: Row(
                                             mainAxisSize: MainAxisSize.max,
                                             mainAxisAlignment:
@@ -244,21 +249,20 @@ class _ProunounWordsPageState extends State<ProunounWordsPage>
                                                     TextStyle(
                                                           fontFamily:
                                                               'Readex Pro',
-                                                          fontSize: 39.0,
+                                                          fontSize: 39,
                                                         ),
                                               ),
                                               Padding(
                                                 padding: EdgeInsetsDirectional
-                                                    .fromSTEB(
-                                                        0.0, 0.0, 8.0, 0.0),
+                                                    .fromSTEB(0, 0, 8, 0),
                                                 child: Text(
-                                                  prounounWordsPagePronounsRecord
+                                                  listViewPronounsRecord
                                                       .signName,
                                                   textAlign: TextAlign.center,
                                                   style: TextStyle(
                                                         fontFamily:
                                                             'Readex Pro',
-                                                        fontSize: 30.0,
+                                                        fontSize: 30,
                                                         fontWeight:
                                                             FontWeight.w600,
                                                       ),
@@ -286,22 +290,20 @@ class _ProunounWordsPageState extends State<ProunounWordsPage>
                   },
                   text: 'Finished Learning',
                   options: GSLButtonOptions(
-                    height: 40.0,
-                    padding:
-                        EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
-                    iconPadding:
-                        EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                    color: GSLAppTheme.of(context).primary,
+                    height: 40,
+                    padding: EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
+                    iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                    color: Color(0xFF4B39EF),
                     textStyle: TextStyle(
                           fontFamily: 'Readex Pro',
                           color: Colors.white,
                         ),
-                    elevation: 3.0,
+                    elevation: 3,
                     borderSide: BorderSide(
                       color: Colors.transparent,
-                      width: 1.0,
+                      width: 1,
                     ),
-                    borderRadius: BorderRadius.circular(8.0),
+                    borderRadius: BorderRadius.circular(8),
                   ),
                 ),
               ],
